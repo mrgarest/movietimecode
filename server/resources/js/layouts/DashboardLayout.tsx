b85f61b058a07855400465a393ae23d4e6bd4e8e
Garest <olehhoncharenko98@gmail.com>
2026-05-28 16:20:01 +0300

    Update DashboardLayout.tsx

--- server/resources/js/layouts/DashboardLayout.tsx
+++ server/resources/js/layouts/DashboardLayout.tsx
@@ -1,14 +1,10 @@
 import SideNavbar from "@/components/dashboard/SideNavbar";
-import { Head } from "@inertiajs/react";
 import { PropsWithChildren } from "react";
 import { Toaster } from 'react-hot-toast';
 
 export default function DashboardLayout({ children }: PropsWithChildren) {
     return (
         <>
-            <Head>
-                <meta name="robots" content="noindex, nofollow" />
-            </Head>
             <div className="container grid grid-cols-1 md:grid-cols-[auto_1fr] gap-8 min-h-screen">
                 <SideNavbar />
                 <main className="pt-6">{children}</main>
